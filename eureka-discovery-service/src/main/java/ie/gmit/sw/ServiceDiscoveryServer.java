508738345b66349e9090e1bf6ec7527e72b4c0a4
EddyCodeIt <g00315079@gmit.ie>
2017-10-10 17:42:58 +0100

    Setting up Eureka Discovery Server

---
+++ eureka-discovery-service/src/main/java/ie/gmit/sw/ServiceDiscoveryServer.java
@@ -0,0 +1,17 @@
+package ie.gmit.sw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class ServiceDiscoveryServer {
+
+    public static void main(String[] args) {
+        // Tell server to look for registration.properties or registration.yml
+        System.setProperty("spring.config.name", "application");
+
+        SpringApplication.run(ServiceDiscoveryServer.class, args);
+    }
+}
