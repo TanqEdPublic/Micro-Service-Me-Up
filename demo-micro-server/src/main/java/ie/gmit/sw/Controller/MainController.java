b58eac7c50995527909cd76875f5129a83be0dbc
KyleFeng <kylezete@gmail.com>
2017-10-17 17:56:39 +0100

    add a demo-micro-server

---
+++ demo-micro-server/src/main/java/ie/gmit/sw/Controller/MainController.java
@@ -0,0 +1,14 @@
+package ie.gmit.sw.Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+
+    @RequestMapping("/hello")
+    public String Hello()
+    {
+        return new String("Hello Eureka `_` ");
+    }
+}
---
+++ demo-micro-server/src/main/java/ie/gmit/sw/DemoMicroServerApplication.java
@@ -0,0 +1,14 @@
+package ie.gmit.sw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class DemoMicroServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoMicroServerApplication.class, args);
+	}
+}
