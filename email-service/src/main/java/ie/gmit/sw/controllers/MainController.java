6e23ce9fb7753637d8ea3ef27ad5735485e9b921
KyleFeng <kylezete@gmail.com>
2017-11-10 10:22:55 +0000

    change request object of mail server

--- email-service/src/main/java/ie/gmit/sw/controllers/MainController.java
+++ email-service/src/main/java/ie/gmit/sw/controllers/MainController.java
@@ -1,5 +1,6 @@
-package ie.gmit.sw.Controllers;
+package ie.gmit.sw.controllers;
 
+import ie.gmit.sw.domain.MailMsg;
 import ie.gmit.sw.services.EmailService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
@@ -18,9 +19,18 @@ public class MainController {
     }
 
     @PostMapping("/sendemail")
-    public String sendEmail(@RequestBody SimpleMailMessage emailMessage){
+    public String sendEmail(@RequestBody MailMsg mailMessage){
+        SimpleMailMessage mail = new SimpleMailMessage();
+        mail.setFrom(mailMessage.getFrom());
+        mail.setTo((String[])mailMessage.getTo().toArray());
+        mail.setSubject(mailMessage.getSubject());
+        mail.setReplyTo(mailMessage.getReplyTo());
+        mail.setCc((String[])mailMessage.getCc().toArray());
+        mail.setBcc((String[])mailMessage.getBcc().toArray());
+        mail.setSentDate(mailMessage.getSentDate());
+        mail.setText(mailMessage.getText());
         try {
-            emailService.sendEmail(emailMessage);
+            emailService.sendEmail(mail);
         }catch (Exception e){
             return e.getMessage();
         }
---
+++ email-service/src/main/java/ie/gmit/sw/domain/MailMsg.java
@@ -0,0 +1,86 @@
+package ie.gmit.sw.domain;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
+
+public class MailMsg implements Serializable {
+
+    private static final long serialVersionUID = 8249147398730391474L;
+
+    private String from;
+    private String replyTo;
+    private List<String> to;
+    private List<String> cc;
+    private List<String> bcc;
+    private Date sentDate;
+    private String subject;
+    private String text;
+
+    public MailMsg() {
+    }
+
+    public String getFrom() {
+        return from;
+    }
+
+    public void setFrom(String from) {
+        this.from = from;
+    }
+
+    public String getReplyTo() {
+        return replyTo;
+    }
+
+    public void setReplyTo(String replyTo) {
+        this.replyTo = replyTo;
+    }
+
+    public List<String> getTo() {
+        return to;
+    }
+
+    public void setTo(List<String> to) {
+        this.to = to;
+    }
+
+    public List<String> getCc() {
+        return cc;
+    }
+
+    public void setCc(List<String> cc) {
+        this.cc = cc;
+    }
+
+    public List<String> getBcc() {
+        return bcc;
+    }
+
+    public void setBcc(List<String> bcc) {
+        this.bcc = bcc;
+    }
+
+    public Date getSentDate() {
+        return sentDate;
+    }
+
+    public void setSentDate(Date sentDate) {
+        this.sentDate = sentDate;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
